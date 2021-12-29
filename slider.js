7efe0042f477485fe2e650162828099e7ae67ec9
bhadmus damilola <90419022+damax-codz@users.noreply.github.com>
2021-12-29 18:38:57 +0100

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,27 @@
+var arr =["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdxrc0W9TKaGPqvX5JdZ-wCilhBAMum91znw&usqp=CAU",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzW7pD-wUMlUchKYexIbALpXvtSvNij8SU7w&usqp=CAU",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyH6RgxMpsi-cqG7fIUuXs9rn9mWSRZtc7Ng&usqp=CAU",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgzJ_Lbwo6--eR_y6FcCh7tRm-yyWBk6fVAg&usqp=CAU"]
+
+var i=0;
+function vic (){
+if(i>=arr.length-1){
+i=arr.length-2
+} 
+i++
+document.getElementById("pics").src=arr[i];
+document.getElementsByTagName("span")[i].style.color="black"
+document.getElementsByTagName("span")[i-1].style.color="rgba(197, 197, 197)"
+}
+
+function voc (){
+if(i==0){
+i+=1
+}    
+i-=1
+
+document.getElementById("pics").src=arr[i]
+
+document.getElementsByTagName("span")[i].style.color="black"
+document.getElementsByTagName("span")[i+1].style.color="rgba(197, 197, 197)"
+}
